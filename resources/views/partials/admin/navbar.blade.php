a64dfc66a0bee0cee1412d590f456f1ac4281001
andarutr <andarutr@gmail.com>
2021-01-15 00:47:10 +0700

    Coding Notifications and Message

--- resources/views/partials/admin/navbar.blade.php
+++ resources/views/partials/admin/navbar.blade.php
@@ -6,13 +6,39 @@
           </ul>
         </form>
         <ul class="navbar-nav navbar-right">
-          
+          <li class="dropdown dropdown-list-toggle"><a href="#" data-toggle="dropdown" class="nav-link nav-link-lg message-toggle beep"><i class="far fa-envelope"></i></a>
+            <div class="dropdown-menu dropdown-list dropdown-menu-right">
+              <div class="dropdown-header">Pesan</div>
+              <div class="dropdown-list-content dropdown-list-message">
+                <a href="javascript:;" class="dropdown-item dropdown-item-unread">
+                  <div class="dropdown-item-avatar">
+                    <img alt="image" src="/img/andarutr.jpg" class="rounded-circle">
+                    <div class="is-online"></div>
+                  </div>
+                  <div class="dropdown-item-desc">
+                    <b>Andaru</b>
+                    <p>Selamat datang di panel admin. Silahkan hubungi saya lebih lanjut, jika mengalami kendala.</p>
+                    <div class="time">{{ date('d F Y') }}</div>
+                  </div>
+                </a>
+              </div>
+            </div>
+          </li>
           <li class="dropdown dropdown-list-toggle"><a href="#" data-toggle="dropdown" class="nav-link notification-toggle nav-link-lg beep"><i class="far fa-bell"></i></a>
             <div class="dropdown-menu dropdown-list dropdown-menu-right">
-              <div class="dropdown-header">Notifications
+              <div class="dropdown-header">Pemberitahuan
               </div>
               <div class="dropdown-list-content dropdown-list-icons">
-                <a href="#" class="dropdown-item dropdown-item-unread">
+                <a href="javascript:;" class="dropdown-item">
+                  <div class="dropdown-item-icon bg-info text-white">
+                    <i class="fas fa-bell"></i>
+                  </div>
+                  <div class="dropdown-item-desc">
+                    Selamat Datang Admin!
+                    <div class="time">{{ date('d F Y') }}</div>
+                  </div>
+                </a>
+                <a href="javascript:;" class="dropdown-item dropdown-item-unread">
                   <div class="dropdown-item-icon bg-success text-white">
                     <i class="fas fa-check"></i>
                   </div>
